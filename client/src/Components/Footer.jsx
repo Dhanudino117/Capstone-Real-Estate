70ce5a3af22d9c500bfd3dd2ff847d994f6c3d7a
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-03-28 11:34:44 +0530

    route error

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -11,40 +11,61 @@ const Footer = () => {
         <div>
           <h2 className="text-2xl font-bold">Havenquix</h2>
           <p className="mt-3 text-gray-600">
-            Discover your perfect space with Havenquix. We connect people with properties
-            that match their lifestyle and aspirations.
+            Discover your perfect space with Havenquix. We connect people with
+            properties that match their lifestyle and aspirations.
           </p>
           {/* Social Icons */}
           <div className="flex space-x-4 mt-4 text-gray-500">
-            <FaInstagram className="text-2xl cursor-pointer hover:text-black" />
-            <FaFacebookF className="text-2xl cursor-pointer hover:text-black" />
-            <FaTwitter className="text-2xl cursor-pointer hover:text-black" />
+            <a href="https://www.instagram.com/accounts/login/?hl=en">
+
+              <FaInstagram className="text-2xl cursor-pointer hover:text-black" />
+            </a>
+            <a href="https://www.facebook.com/">
+              <FaFacebookF className="text-2xl cursor-pointer hover:text-black" />
+            </a>
+            <a href="https://x.com/">
+              <FaTwitter className="text-2xl cursor-pointer hover:text-black" />
+            </a>
           </div>
         </div>
 
         {/* Quick Links */}
-        <div  className="">
+        <div className="">
           <h3 className="font-semibold text-lg">Quick Links</h3>
-          
+
           <ul className="mt-3 space-y-2 text-gray-600">
-            <Link to='/' className="cursor-pointer hover:text-black">Home</Link> <br />
-            <Link to='/about' className="cursor-pointer hover:text-black">About Us</Link> <br />
-            <Link to='/testimonials' className="cursor-pointer hover:text-black">Testimonials</Link><br />
-            <Link to='/contact' className="cursor-pointer hover:text-black">Contacts</Link>
+            <Link to="/" className="cursor-pointer hover:text-black">
+              Home
+            </Link>{" "}
+            <br />
+            <Link to="/about" className="cursor-pointer hover:text-black">
+              About Us
+            </Link>{" "}
+            <br />
+            <Link
+              to="/testimonials"
+              className="cursor-pointer hover:text-black"
+            >
+              Testimonials
+            </Link>
+            <br />
+            <Link to="/contact" className="cursor-pointer hover:text-black">
+              Contacts
+            </Link>
           </ul>
         </div>
 
-        
         <div>
           <h3 className="font-semibold text-lg">Legal</h3>
           <ul className="mt-3 space-y-2 text-gray-600">
             <li className="cursor-pointer hover:text-black">Privacy Policy</li>
-            <li className="cursor-pointer hover:text-black">Terms of Service</li>
+            <li className="cursor-pointer hover:text-black">
+              Terms of Service
+            </li>
             <li className="cursor-pointer hover:text-black">Cookie Policy</li>
           </ul>
         </div>
 
-        
         <div>
           <h3 className="font-semibold text-lg">Contact</h3>
           <div className="mt-3 space-y-2 text-gray-600">
