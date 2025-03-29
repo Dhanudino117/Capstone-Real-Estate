7e53a22ad9b936b5c382d37a4541b35f28719c15
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-03-29 21:35:14 +0530

    Property Listing Schema added

---
+++ server/Models/Properties.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const PropertySchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    address: { type: true, required: true },
+    regularprice: { type: Number, required: true },
+    discountPrice: { type: Number, required: true },
+    bathrooms: { type: Number, required: true },
+    bedrooms: { type: Number, required: true },
+    furnished: { type: Boolean, required: true },
+    type: { type: String, required: true },
+    parking: { type: Boolean, required: true },
+    offer: { type: Boolean, required: true },
+    location: { type: String, required: true },
+    images: { type: Array, required: true },
+    UserRef: { tyoe: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", PropertySchema);
+
+export default Listing;
