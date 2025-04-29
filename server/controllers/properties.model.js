fc12cd376ae38e5941b4c0c7f5028b4641039cc0
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-04-30 00:59:04 +0530

    Get-endpoint-updated

--- server/controllers/properties.model.js
+++ server/controllers/properties.model.js
@@ -1,4 +1,5 @@
-import Listing from '../Models/Properties.model.js'
+import Listing from "../Models/Properties.model.js";
+import {validationResult} from 'express-validator'
 
 export const getAllProperties = async (req, res, next) => {
   try {
@@ -14,14 +15,19 @@ export const getAllProperties = async (req, res, next) => {
 };
 
 export const createProperty = async (req, res, next) => {
-    try {
-      const property = new Listing(req.body); 
-      const savedProperty = await property.save(); 
-      res.status(201).json({
-        success: true,
-        data: savedProperty,
-      });
-    } catch (error) {
-      next(error); 
-    }
-  };
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success: false, errors: errors.array() });
+  }
+
+  try {
+    const property = new Listing(req.body);
+    const savedProperty = await property.save();
+    res.status(201).json({
+      success: true,
+      data: savedProperty,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
