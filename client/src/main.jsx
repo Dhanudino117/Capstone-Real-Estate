8616a51f8d13eab4eac840a61c42803275b070a3
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-05-06 17:25:56 +0530

    Added vercel.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId="927666537387-krq5q195so0ii1n9dmq45c594su8e3ru.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+    ;
+  </StrictMode>
+);
--- server/Models/User.model.js
+++ server/Models/User.model.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
-      
     },
     email: {
       type: String,
@@ -15,6 +14,10 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+    },
+    googleId: {
+      type: String,
+      unique: true,
     },
   },
   { timestamps: true }
